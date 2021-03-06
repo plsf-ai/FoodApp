df7423601aad35b679f85b7aac60574e68397238
unknown <pasanjay119@gmail.com>
2021-03-06 14:20:18 +0530

    this is first commit

---
+++ android/app/src/main/kotlin/com/example/FoodAp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FoodAp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static String key = "";
+  static String secret = "";
+  static String url = "https://dev-kolk.pantheonsite.io/wp-json/wc/v3/";
+  static String customerURL = "customers";
+}
